e8cad6d411b0a6c97997227e4522babb0702e19f
Thomas Levine <occurrence@thomaslevine.com>
2013-10-27 11:04:59 +0000

    code

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 download:
 	test -e schedule.json || wget -O schedule.json http://schedule.mozillafestival.org/schedule
+unnest:
+	python2 reshape.py
--- reshape.py
+++ reshape.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
 import json
+import pandas
 
 nested = json.load(open('schedule.json'))
+
+def unnest(nested):
+    for k,v in nested['schedule'].items():
+        for session in v.values():
+            session['track'] = k
+            yield session
+
+flat = list(unnest(nested))
+
+json.dump(flat, open('flat.json', 'w'))
+pandas.DataFrame(flat).to_csv('flat.csv', encoding = 'utf-8')
